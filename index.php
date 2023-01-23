0ae017b9d1510097e783b8308fbfd807d5331398
yoga0123456789 <yogapratama@teknokrat.ac.id>
2023-01-23 20:23:45 +0700

    YogaPratama: membuat file index.php dan content.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>SEVEN SKY STORE</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>SEVEN SKY STORE</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by SEVEN SKY STORE</a></p>
+  </div>
+
+</body>
+
+</html>
