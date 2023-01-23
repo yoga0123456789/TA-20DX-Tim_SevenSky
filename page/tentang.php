8b657b8498a5223a50e856d59bcaab3babb0f1cd
yoga0123456789 <yogapratama@teknokrat.ac.id>
2023-01-23 19:42:51 +0700

    YogaPratama: mebuat file navbar.php, tentang.php dan logout.php

---
+++ page/tentang.php
@@ -0,0 +1,58 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Global Computer, Kedaton, Bandar Lampung</h1>
+  <img src="img/LGO GLOBALNEW.jpg" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>Nama Toko</b></li>
+        </td>
+        <td>: Global Computer</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>email</b></li>
+        </td>
+        <td>: globalcomputerkedaton158@gmail.com</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>No. Telp</b></li>
+        </td>
+        <td>: 0822 7890 1245</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Alamat</b></li>
+        </td>
+        <td>:Jl. Teuku Umar No. 158-159, Kedaton, Bandar Lampung</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Produk</b></li>
+        </td>
+        <td>: Komputer & aksesoris komputer</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
